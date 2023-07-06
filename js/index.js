30b352f2368e78b21e929a3d7c8fa6aade43cba8
jsunbin <jsnb6n@gmail.com>
2023-07-06 15:41:18 +0900

    ✨ 메모 수정 기능 추가

--- js/index.js
+++ js/index.js
@@ -1,20 +1,18 @@
-
-let savedMemoList = []
+let savedMemoList = [];
 function checkLocalStorage() {
-  const localSavedMemo = window.localStorage.getItem('savedMemo');
+  const localSavedMemo = window.localStorage.getItem("savedMemo");
 
-  if(localSavedMemo) {
+  if (localSavedMemo) {
     savedMemoList = JSON.parse(localSavedMemo);
     isMemo = true;
-    showOldMemo(savedMemoList)
-    
+    showOldMemo(savedMemoList);
   }
 }
 
 function showOldMemo(savedMemoList) {
   const container = document.querySelector(".saved-paper");
   let memoList = [];
-  savedMemoList.forEach(el => {
+  savedMemoList.forEach((el) => {
     const newMemo = `
       <div class="wrap-paper" data-id=${el.id}>
         <div class="paper-area">
@@ -28,7 +26,7 @@ function showOldMemo(savedMemoList) {
             <div class="square"></div>
           </div>
           <div class="memo-area">
-            <textarea id="editMemo" cols="" rows="" placeholder="간단하게 메모를 작성해보세요">${el.memo}</textarea>
+            <div class="memo-contents">${el.memo}</div>
           </div>
           <div class="memo-footer">
             <p class="update-time">
@@ -36,6 +34,7 @@ function showOldMemo(savedMemoList) {
               ${el.date}
             </p>
             <div class="edit-area">
+              <button class="edit-btn" type="button"><img src="./image/eraser.png"></button>
               <button class="delete-btn" type="button"><img src="./image/bin.png" alt="삭제하기"></button>
             </div>
           </div>
@@ -44,18 +43,15 @@ function showOldMemo(savedMemoList) {
     `;
     memoList.push(newMemo);
   });
-  container.innerHTML = memoList.reverse().join("")
-  
+  container.innerHTML = memoList.reverse().join("");
 }
 
 function saveMemo(event) {
-  const newContentEl= document.querySelector('.first-memo #editMemo');
-
-  console.log(event);
+  const newContentEl = document.querySelector(".first-memo #editMemo");
 
   // 메모 리스트 생성하기
   makeMemoList(newContentEl);
-  
+
   // 2. 새로운 메모 생성하기
   createNewMemo();
 
@@ -67,8 +63,8 @@ function makeMemoList(newContentEl) {
   // 1. 메모 내용 가져오기
 
   // 1-0. id:
-  const memoId = getCurrentTimeInSeconds()
-  
+  const memoId = getCurrentTimeInSeconds();
+
   // 1-1. 작성 시간
   const lastModifiedDate = formattingTime();
 
@@ -76,16 +72,20 @@ function makeMemoList(newContentEl) {
   const newMemoContent = newContentEl.value;
 
   // 1-3. 메모 리스트 만들기: savedMemoList
-  savedMemoList.push({id: memoId, memo: newMemoContent, date: lastModifiedDate});
+  savedMemoList.push({
+    id: memoId,
+    memo: newMemoContent,
+    date: lastModifiedDate,
+  });
 
   // 1-4. 로컬스토리지에 메모리스트 저장하기: {"savedMemo": "${savedMemoList}"}
-  window.localStorage.setItem('savedMemo', JSON.stringify(savedMemoList));
+  window.localStorage.setItem("savedMemo", JSON.stringify(savedMemoList));
 }
 
 function getCurrentTimeInSeconds() {
   const currentTime = new Date();
   const seconds = Math.floor(currentTime.getTime() / 1000);
-  return seconds.toString().padStart(10, '0');
+  return seconds.toString().padStart(10, "0");
 }
 
 function formattingTime() {
@@ -96,13 +96,13 @@ function formattingTime() {
   let hour = String(now.getHours()).padStart(2, "0");
   let minutes = String(now.getMinutes()).padStart(2, "0");
 
-  return `${year}/${month}/${day} ${hour}:${minutes}`
+  return `${year}/${month}/${day} ${hour}:${minutes}`;
 }
 
 function createNewMemo() {
   const container = document.querySelector(".saved-paper");
   let memoList = [];
-  savedMemoList.forEach(el => {
+  savedMemoList.forEach((el) => {
     const newMemo = `
       <div class="wrap-paper" data-id=${el.id}>
         <div class="paper-area">
@@ -116,7 +116,7 @@ function createNewMemo() {
             <div class="square"></div>
           </div>
           <div class="memo-area">
-            <textarea id="editMemo" cols="" rows="" placeholder="간단하게 메모를 작성해보세요">${el.memo}</textarea>
+            <div class="memo-contents">${el.memo}</div>
           </div>
           <div class="memo-footer">
             <p class="update-time">
@@ -124,6 +124,7 @@ function createNewMemo() {
               ${el.date}
             </p>
             <div class="edit-area">
+              <button class="edit-btn" type="button"><img src="./image/eraser.png"></button>
               <button class="delete-btn" type="button"><img src="./image/bin.png" alt="삭제하기"></button>
             </div>
           </div>
@@ -133,7 +134,7 @@ function createNewMemo() {
     memoList.push(newMemo);
   });
 
-  container.innerHTML = memoList.reverse().join("")
+  container.innerHTML = memoList.reverse().join("");
 }
 
 function clearMemo(newContent) {
@@ -143,20 +144,95 @@ function clearMemo(newContent) {
 // 삭제하기
 function deleteMemo(event) {
   event.preventDefault();
-  const deleteBtn = event.target.closest('.delete-btn');
+  const deleteBtn = event.target.closest(".delete-btn");
   if (deleteBtn) {
-    const memoId = deleteBtn.closest('div.wrap-paper').getAttribute('data-id');
+    const memoId = deleteBtn.closest("div.wrap-paper").getAttribute("data-id");
     savedMemoList = savedMemoList.filter((memo) => memo.id !== memoId);
-    window.localStorage.setItem('savedMemo', JSON.stringify(savedMemoList));
+    window.localStorage.setItem("savedMemo", JSON.stringify(savedMemoList));
     showOldMemo(savedMemoList);
   }
 }
 
+// 수정하기
+function editMemo(event) {
+  event.preventDefault();
 
-window.addEventListener('load', checkLocalStorage);
+  // 수정하기 버튼을 누르면
+  // 1. event에서 data-id 가져오기
+  const editBtn = event.target.closest(".edit-btn");
+  const memoId = editBtn.closest("div.wrap-paper").getAttribute("data-id");
+
+  const targetMemo = savedMemoList.find((memo) => memo.id === memoId);
+  // 2. edit-memo에 자식요소로 해당하는 메모 정보가 담겨 있는 textAREA가 있는 html return
+
+  const container = document.querySelector(".edit-memo");
+
+  container.classList.add("active");
+  container.innerHTML = `
+    <div class="wrap-paper" data-id="${memoId}">
+    <div class="paper-area">
+      <div class="memo-head">
+        <div class="square"></div>
+        <div class="square"></div>
+        <div class="square"></div>
+        <div class="square"></div>
+        <div class="square"></div>
+        <div class="square"></div>
+        <div class="square"></div>
+      </div>
+      <div class="memo-area">
+        <textarea class="memo-contents">${targetMemo.memo}</textarea>
+      </div>
+      <div class="memo-footer">
+        <p class="update-time">
+          <span class="a11y-hidden">작성 시간 : </span>
+          ${targetMemo.date}
+        </p>
+        <div class="edit-area">
+          <button class="save-btn" type="button">
+            <img src="./image/check.png" />저장하기
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+
+  const saveBtn = document.querySelector(".edit-memo .save-btn");
+  saveBtn.addEventListener("click", editSave);
+}
+
+function editSave(event) {
+  event.preventDefault();
+
+  const saveBtn = event.target.closest(".save-btn");
+  if (saveBtn) {
+    const memoId = saveBtn.closest("div.wrap-paper").getAttribute("data-id");
+    const targetMemo = savedMemoList.find((memo) => memo.id === memoId);
+    if (targetMemo) {
+      const editedMemoContent = document.querySelector(
+        ".edit-memo .memo-contents"
+      );
+      targetMemo.memo = editedMemoContent.value;
+    }
+    const container = document.querySelector(".edit-memo");
+
+    container.classList.remove("active");
+    window.localStorage.setItem("savedMemo", JSON.stringify(savedMemoList));
+    showOldMemo(savedMemoList);
+  }
+}
+
+window.addEventListener("load", checkLocalStorage);
 
 const saveBtn = document.querySelector(".first-memo button");
-saveBtn.addEventListener('click', saveMemo)
+saveBtn.addEventListener("click", saveMemo);
 
 const container = document.querySelector(".saved-paper");
-container.addEventListener('click', deleteMemo);
+container.addEventListener("click", function (event) {
+  if (event.target.parentElement.classList.contains("delete-btn")) {
+    deleteMemo(event);
+  } else if (event.target.parentElement.classList.contains("edit-btn")) {
+    editMemo(event);
+  }
+});
